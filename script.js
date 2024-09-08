f838ac1c3417e10e0ad4e5bc84c0d4bf0c46825e
Rehanispro75 <rehanshaikh218053@gmail.com>
2024-09-08 19:42:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+var student = {
+
+name : "Rehan",
+grade : 7,
+school : "Crescent High school"
+
+
+
+
+}
+
+document.getElementById("demo").innerHTML= student.name + "<br>"+ student.grade+ "<br>"+
+student.school
+
+
+var num = Math.random()
+
+var num2 = 10000*num
+
+var num3 = Math.floor(num2)  
+
+
+document.getElementById("div1").innerHTML= num3
+
+
+
+
